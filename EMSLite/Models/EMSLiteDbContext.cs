6d34248d9bb8b662a7e1ce3959dedb1f667ccedd
karthikchittabathini <chittabathinikarthik@gmail.com>
2023-04-28 12:30:20 +0530

    EMS

---
+++ EMSLite/Models/EMSLiteDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EMSLite.Models
+{
+    public partial class EMSLiteDbContext : DbContext
+    {
+        public EMSLiteDbContext()
+        {
+        }
+
+        public EMSLiteDbContext(DbContextOptions<EMSLiteDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=KARTHI\\SQLEXPRESS;Initial Catalog=EMSLite;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasKey(e => e.EmpId)
+                    .HasName("PK__Employee__AF2DBA7912F3738F");
+
+                entity.Property(e => e.EmpId).ValueGeneratedNever();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ EMSLite/Models/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMSLite.Models
+{
+    [Table("Employee")]
+    public partial class Employee
+    {
+        [Key]
+        [Column("EmpID")]
+        public int EmpId { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string FirstName { get; set; } = null!;
+        [StringLength(50)]
+        [Unicode(false)]
+        public string LastName { get; set; } = null!;
+        [StringLength(10)]
+        [Unicode(false)]
+        public string Gender { get; set; } = null!;
+        [Column(TypeName = "date")]
+        public DateTime DateofBirth { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Email { get; set; } = null!;
+        [StringLength(20)]
+        [Unicode(false)]
+        public string MobileNo { get; set; } = null!;
+        [StringLength(255)]
+        [Unicode(false)]
+        public string CurrentAddress { get; set; } = null!;
+        [Column(TypeName = "date")]
+        public DateTime DateofJoining { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Position { get; set; } = null!;
+        [StringLength(50)]
+        [Unicode(false)]
+        public string DepartmentName { get; set; } = null!;
+        [StringLength(50)]
+        [Unicode(false)]
+        public string Manager { get; set; } = null!;
+    }
+}
